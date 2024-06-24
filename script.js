d7205a1e60cf4c2ab0154767e5384a4985657311
Abhishek-coder2001 <abhishek800.roy@gmail.com>
2024-06-24 16:33:37 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const title = document.getElementById("title");
+const desc = document.getElementById("desc")
+const sname = document.getElementById("name")
+
+
+
+title.innerText = "My Crush, My one sided ❤️, My cutiepie<3";
+
+desc.innerText = "I cannot tell you ho much I love/like you. I know you don't love me, but their is something called- `One Sided Love❤️` . Which is beautifull, no worries, no commitments, just look at you, fell for you and smile❤️. I hope our friendship and our bond grows stronger and deeper. By the way➡️ Bohot gyan de diye,apne dill ki bat bol diya, asli bat batana vul giya .➡️ HAPPY BIRTHDAY 🎉, Many many happy returns of the day 🎂. Stay blessed, Stay happy, Keep smile always.▶️ Sayad main others ladko ki tarha apni bat na bol paya, apna feelisng express nahi kar paya, but jo v kiya dill se kiya 💕. At the end-> PARTY TO BANTI HAI..!";
+
+sname.innerText = "Abhishek";
